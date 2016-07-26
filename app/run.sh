09a427863da9a10085e34dc8c9565fa0deed624c
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2016-07-26 15:42:47 +0200

    cfg fixs

--- app/run.sh
+++ app/run.sh
@@ -6,9 +6,6 @@ echo "Hostview Upload starting in '$NODE_ENV' environment"
 if [[ $TEST && ${TEST-x} ]] ; then
     echo "Running all tests ..."
     exec npm test
-elif [[ $DEBUG && ${DEBUG-x} ]] ; then
-    echo "Starting the debugger ..."
-    exec npm run-script debug
 else
     exec npm start
 fi
